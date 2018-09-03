577552eb9aa0a67892b93484b3ea66702051667d
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-03 22:17:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,7 @@
 def prime?(integer)
-  i = []
-  prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-  if integer == 0 || 1
-    return false
+  #i = []
+  #prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    return false if integer < 2
     return true if integer == 3 || integer == 2
         if (2...integer-1).any?{|i| integer % i == 0}
           false
