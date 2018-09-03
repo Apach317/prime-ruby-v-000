f36d5dbb3768c540145e8dfb4b46f08918a3d7a7
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-03 22:19:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,9 @@ def prime?(integer)
   #prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
   if integer < 2
       return false
-  if integer == 3 || integer == 2
+  elsif integer == 3 || integer == 2
         return true
-        if (2...integer-1).any?{|i| integer % i == 0}
+  elsif (2...integer-1).any?{|i| integer % i == 0}
           return false
              else
                return true
