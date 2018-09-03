d706f6d9a8191b8d48463e17ae1f778c42e9417a
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-03 22:07:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(integer)
   prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
   if integer == 0 || 1
     return false
-  elsif
+  else
     return true
   end
 end
